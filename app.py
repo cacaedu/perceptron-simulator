6b6778ada4dc1a844ba0b52dbec70669b4cb91f2
cacaedu <33465608+cacaedu@users.noreply.github.com>
2026-04-05 18:46:44 -0300

    Move circle position in plot
    Adjusted the position of the circle in the plot.

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ with col2:
     fig, ax = plt.subplots()
     
     # desenhar círculo (neurônio)
-    circulo = plt.Circle((0.58, 0.5), 0.1, fill=False)
+    circulo = plt.Circle((0.6, 0.5), 0.1, fill=False)
     ax.add_patch(circulo)
     
     # símbolo Σ
