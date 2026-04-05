646af0d10c1a8e727f1124a22271004f8039f376
cacaedu <33465608+cacaedu@users.noreply.github.com>
2026-04-05 15:46:29 -0300

    Add sliders for weight inputs in app.py

--- app.py
+++ app.py
@@ -16,3 +16,23 @@ st.write({
     "ruim": int(ruim),
     "neutro": int(neutro)
 })
+
+st.subheader("Pesos")
+
+peso_bom = st.slider("Peso do Bom", -2.0, 2.0, 1.0, 0.1)
+peso_ruim = st.slider("Peso do Ruim", -2.0, 2.0, -1.0, 0.1)
+peso_neutro = st.slider("Peso do Neutro", -2.0, 2.0, 0.0, 0.1)
+
+st.write("Estado atual:")
+st.write({
+    "inputs": {
+        "bom": int(bom),
+        "ruim": int(ruim),
+        "neutro": int(neutro)
+    },
+    "pesos": {
+        "bom": peso_bom,
+        "ruim": peso_ruim,
+        "neutro": peso_neutro
+    }
+})
